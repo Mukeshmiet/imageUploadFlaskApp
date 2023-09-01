d27bd1051218667b8682b344f4dc97b5f9b78eac
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-09-02 04:32:03 +0530

    key based JWT authentication system added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename     # secure_filename is used to sanitize and secure filename before storing it
 import os
+import jwt
 
 
 app = Flask(__name__)
@@ -8,6 +9,32 @@ app = Flask(__name__)
 # uploaded Images folder path: 'C:/Users/Dell/Downloads/userImages'  or 'userImages'
 app.config['UPLOAD_FOLDER'] = 'userImages'       
 
+# It Generates 'app_secret_key', a random secure secret key with 32 bytes (256 bits)
+#import secrets
+#SECRET_KEY = secrets.token_hex(32)
+
+SECRET_KEY = 'b9dd0c9baeccb0cd3af3c9e14f26d405653e54afc742f46e9d7fb03a4e3e63b4'   
+
+# Create a route to generate a JWT token
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()   # a JSON request with user data
+    if data['username'] == 'flaskApp' and data['password'] == 'App@123':
+        token = jwt.encode({'user': data['username']}, SECRET_KEY, algorithm='RS256')
+        return jsonify({'token': token.decode('UTF-8')})
+    return jsonify({'message': 'Authentication failed'}), 401
+
+#  Create a protected route that requires a valid JWT token to access
+@app.route('/protected', methods=['GET'])
+def protected():
+    token = request.headers.get('Authorization')
+    try:
+        jwt.decode(token, SECRET_KEY, algorithms=['RS256'])
+        return jsonify({'message': 'Access granted'})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'message': 'Token has expired'}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({'message': 'Invalid token'}), 401
 
 # home page route  
 @app.route('/')
