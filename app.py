0952e6da26d4807d78342943d4d0bb3efd216bbf
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-09-02 03:14:37 +0530

    image uploading and displaying defined

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename     # secure_filename is used to sanitize and secure filename before storing it
+import os
+
 
 app = Flask(__name__)
 
+# uploaded Images folder path: 'C:/Users/Dell/Downloads/userImages'  or 'userImages'
+app.config['UPLOAD_FOLDER'] = 'userImages'       
+
 @app.route('/')
-def home():
+def upload():
     return render_template('upload.html')
 
-@app.route('/image')
-def about():
-    return render_template('image.html', filename='image.jpg')
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    file = request.files['image']
+    # secure_filename is used to sanitize and secure filename before storing it
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    print('Image successfully uploaded')
+    return redirect(url_for('image', filename=filename))
 
-@app.route('/notexist')
-def about():
-    return render_template('notexist.html')
+@app.route('/image/<filename>')
+def image(filename):
+    if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+        return render_template('image.html', filename=filename)
 
-@app.errorhandler(Exception)
-def handle_error(error):
-    print(error)
-    return render_template('error.html'), 404
 
 if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,7 @@
 console.log('scripts.js loaded');
+
+document.getElementById("file-input").addEventListener("change", function () {
+    const fileNameDisplay = document.getElementById("file-name-display");
+    const fileName = this.files[0].name;
+    fileNameDisplay.textContent = `Selected file: ${fileName}`;
+});
