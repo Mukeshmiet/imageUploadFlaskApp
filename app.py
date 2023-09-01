0585bba3be54b8afc5f59c473721990cd92e298b
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-09-02 01:54:45 +0530

    templates and static files created

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hi, I am Mukesh Kumar'
+    return render_template('upload.html')
 
-@app.route('/about')
+@app.route('/image')
 def about():
-    return 'On the way to become a Python Developer'
+    return render_template('image.html', filename='image.jpg')
+
+@app.route('/notexist')
+def about():
+    return render_template('notexist.html')
+
+@app.errorhandler(Exception)
+def handle_error(error):
+    print(error)
+    return render_template('error.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
